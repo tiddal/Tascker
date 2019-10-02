60d00a77d8bbd9d889df045ca059fdaeb8efc093
tiddal <tiddal@sapo.pt>
2019-10-02 19:45:54 +0100

    correcao de erros no modelo Project, configurada ligação com BD

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const path = require('path'),
-    env = require('dotenv').config(),  
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'), 
-    methodOverride = require('method-override'),
-    port = process.env.PORT || 3000,
-    express = require('express'),
-    app = express();
+	env = require('dotenv').config(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	methodOverride = require('method-override'),
+	port = process.env.PORT || 3000,
+	express = require('express'),
+	app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose
+	.connect(process.env.DBURL, {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log('Successfully connected to the database');
+	})
+	.catch((err) => {
+		console.log(`Failed to connect to the database: ${err.message}`);
+	});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 app.set(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('Landing');
+	res.render('Landing');
 });
 
 app.listen(port, process.env.IP, () => {
-    console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
+
+const projectRoutes = require('./routes/projects');
+app.use(projectRoutes);
--- models/Project.js
+++ models/Project.js
@@ -1,14 +1,14 @@
-const mongoose = require('moongose');
+const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    author: String,
-    pin: String,
-    createdAt: {type: Date, default: Date.now},
-    finishedAt: {type: Date},
-    team: [String],
-    tasks: [{type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
+	name: String,
+	description: String,
+	author: String,
+	pin: String,
+	createdAt: { type: Date, default: Date.now },
+	finishedAt: { type: Date },
+	team: [String],
+	tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
 });
 
 const Project = mongoose.model('Project', projectSchema);
